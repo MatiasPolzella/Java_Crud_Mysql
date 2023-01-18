1e4a68d358a1174dca9a2b637d994fcc278a1702
MatiasPolzella <matiaspolzella97@gmail.com>
2023-01-18 10:33:51 -0300

    final changes

---
+++ src/main/java/com/mycompany/java_crud_mysql/CConexion.java
@@ -0,0 +1,37 @@
+package com.mycompany.java_crud_mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MATIAS
+ */
+public class CConexion {
+    
+    Connection conectar = null;
+    
+    String usuario = "root";
+    String contrasenia = "Independiente97";
+    String bd = "bdescuela";
+    String ip = "localhost";
+    String puerto = "3306";
+    
+    String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+    
+    public Connection estableceConexion () {
+        
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
+            //JOptionPane.showMessageDialog(null,"La conexion ha sido realizada con exito");
+            
+        }catch(Exception e) {
+            JOptionPane.showMessageDialog(null,"Error al conectarse a la base de datos, error: " +  e.toString());
+        }
+        return conectar;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/java_crud_mysql/inicio.java
@@ -0,0 +1,15 @@
+package com.mycompany.java_crud_mysql;
+
+/**
+ *
+ * @author MATIAS
+ */
+public class inicio {
+    
+    public static void main(String[] args) {
+        
+        FormAlumno objetoFormulario = new FormAlumno();
+        objetoFormulario.setVisible(true);
+               
+    }
+}
